c81e6d81f9fcd78a1b16394a209c1327faa960ac
Mark Suarez <morphieus111@gmail.com>
2025-11-10 14:54:49 +0800

    member

--- member_management.Designer.cs
+++ member_management.Designer.cs
@@ -34,6 +34,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.btnBookMng = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -42,8 +43,9 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(1, -2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(951, 516);
+            this.pictureBox1.Size = new System.Drawing.Size(713, 419);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -51,49 +53,65 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(258, 180);
+            this.dataGridView1.Location = new System.Drawing.Point(194, 146);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(628, 264);
+            this.dataGridView1.Size = new System.Drawing.Size(471, 214);
             this.dataGridView1.TabIndex = 1;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(326, 131);
+            this.textBox1.Location = new System.Drawing.Point(244, 106);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 22);
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
             this.textBox1.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(270, 137);
+            this.label1.Location = new System.Drawing.Point(202, 111);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 3;
             this.label1.Text = "Search";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(508, 130);
+            this.button1.Location = new System.Drawing.Point(381, 106);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 4;
             this.button1.Text = "search";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // btnBookMng
+            // 
+            this.btnBookMng.Location = new System.Drawing.Point(68, 81);
+            this.btnBookMng.Name = "btnBookMng";
+            this.btnBookMng.Size = new System.Drawing.Size(101, 43);
+            this.btnBookMng.TabIndex = 5;
+            this.btnBookMng.Text = "Book Management";
+            this.btnBookMng.UseVisualStyleBackColor = true;
+            this.btnBookMng.Click += new System.EventHandler(this.btnBookMng_Click);
+            // 
             // member_management
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(948, 515);
+            this.ClientSize = new System.Drawing.Size(711, 418);
+            this.Controls.Add(this.btnBookMng);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "member_management";
             this.Text = "member_management";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -110,5 +128,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnBookMng;
     }
 }
--- member_management.cs
+++ member_management.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace librarysystem
 {
@@ -15,11 +16,54 @@ namespace librarysystem
         public member_management()
         {
             InitializeComponent();
+            loadUsers("");
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            loadUsers(textBox1.Text.Trim());
+            textBox1.Clear();
+        }
 
+        private void loadUsers(string searchTerm)
+        {
+            try
+            {
+                string query = "SELECT user_id, role, username, email, fullname, date_registered, borrowed_books, returned_books, fines_fees FROM users";
+                if (!string.IsNullOrEmpty(searchTerm))
+                {
+                    query += " WHERE user_id LIKE @search OR " +
+                             "username LIKE @search OR " +
+                             "fullname LIKE @search OR " +
+                             "email LIKE @search";
+                }
+                using (MySqlConnection conn = new MySqlConnection(connector.connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    if (!string.IsNullOrEmpty(searchTerm))
+                    {
+                        cmd.Parameters.AddWithValue("@search", "%" + searchTerm + "%");
+                    }
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    dataGridView1.DataSource = dt;
+                    dataGridView1.ClearSelection();
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show("An error occured while loading user data: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnBookMng_Click(object sender, EventArgs e)
+        {
+            manage_books manageBooksForm = new manage_books();
+            manageBooksForm.Show();
+            this.Hide();
         }
     }
 }
